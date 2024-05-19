bae906b97128a1166b292648c88f626a0646a660
leonvodacom <leon.mwandiringa@vcontractor.co.za>
2024-05-19 16:36:01 +0200

    authorizer support

--- authorizer.tf
+++ authorizer.tf
@@ -1,7 +1,6 @@
 resource "aws_api_gateway_authorizer" "default" {
   count = var.authorizers != null ? length(var.authorizers) : 0
   name                   = var.authorizers[count.index].name
-  authorizer_uri = var.authorizers[count.index].authorizer_uri
   rest_api_id            = aws_api_gateway_rest_api.default.id
   type = var.authorizers[count.index].type
   provider_arns = var.authorizers[count.index].provider_arns
